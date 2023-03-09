b0d2fc826aae4a6cde36c19c08d2e94f3ef0364d
Francois Robbertze <francoisdprob@gmail.com>
2023-03-09 16:45:24 +0200

    Changed output dir

--- php/generate.php
+++ php/generate.php
@@ -1,93 +1,91 @@
+// Generate Variations
+
 <?php
-generateCSV();
-function generateCSV()
-{
-    $variations = $_POST['variations'];
-    if ($variations > 1000500) {
-        header("Location: /index.php?invalid=true");
-        exit();
-    }
-    $today = new DateTime(date('Y-m-d'));
-    $names = array(
-        "Bianca",
-        "Jacques",
-        "Francois",
-        "Vance",
-        "Pranav",
-        "Thomas",
-        "Carl",
-        "Nico",
-        "Luis",
-        "Sean",
-        "Sasha Cohen",
-        "Erica",
-        "Megan",
-        "Katya",
-        "Annalette",
-        "Phillip",
-        "Ricus",
-        "Jowan",
-        "Amiel",
-        "Genise"
-    );
-    $surnames = array(
-        "Robbertze",
-        "Harmon",
-        "Bolton",
-        "Guerrero",
-        "Quinn",
-        "Hubbard",
-        "Moore",
-        "Mahoney",
-        "Abbott",
-        "Spears",
-        "Pierce",
-        "Hood",
-        "Cline",
-        "Henderson",
-        "Barnard",
-        "Jensen",
-        "Kirk",
-        "Huynh",
-        "Acosta",
-        "Mcclain"
-    );
+$variations = $_POST['variations'];
+if ($variations > 1000500) {
+    header("Location: /index.php?invalid=true");
+    exit();
+}
+$today = new DateTime(date('Y-m-d'));
+$names = array(
+    "Bianca",
+    "Jacques",
+    "Francois",
+    "Vance",
+    "Pranav",
+    "Thomas",
+    "Carl",
+    "Nico",
+    "Luis",
+    "Sean",
+    "Sasha Cohen",
+    "Erica",
+    "Megan",
+    "Katya",
+    "Annalette",
+    "Phillip",
+    "Ricus",
+    "Jowan",
+    "Amiel",
+    "Genise"
+);
+$surnames = array(
+    "Robbertze",
+    "Harmon",
+    "Bolton",
+    "Guerrero",
+    "Quinn",
+    "Hubbard",
+    "Moore",
+    "Mahoney",
+    "Abbott",
+    "Spears",
+    "Pierce",
+    "Hood",
+    "Cline",
+    "Henderson",
+    "Barnard",
+    "Jensen",
+    "Kirk",
+    "Huynh",
+    "Acosta",
+    "Mcclain"
+);
 
-    if (file_exists("output")) {
-        $file = fopen("output/output.csv", "w");
-    } else {
-        mkdir("output");
-        $file = fopen("output/output.csv", "w");
-    }
-    //Header
-    fputcsv($file, [
-        "Id",
-        "Name",
-        "Surname",
-        "Initials",
-        "Age",
-        "DateOfBirth"
-    ]);
+if (file_exists("../output")) {
+    $file = fopen("../output/output.csv", "w");
+} else {
+    mkdir("../output");
+    $file = fopen("../output/output.csv", "w");
+}
+//Header
+fputcsv($file, [
+    "Id",
+    "Name",
+    "Surname",
+    "Initials",
+    "Age",
+    "DateOfBirth"
+]);
 
 
-    $data = [];
-    $iCount = 0;
-    while ($iCount < $variations) {
-        $name = $names[array_rand($names)];
-        $surname = $surnames[array_rand($surnames)];
-        $dob = date("Y-m-d", rand(strtotime("01-01-1940"), strtotime("01-01-2005")));
+$data = [];
+$iCount = 0;
+while ($iCount < $variations) {
+    $name = $names[array_rand($names)];
+    $surname = $surnames[array_rand($surnames)];
+    $dob = date("Y-m-d", rand(strtotime("01-01-1940"), strtotime("01-01-2005")));
 
-        $age = calculateage($dob, $today);
-        $hash = $name . $surname . $age . $dob;
+    $age = calculateage($dob, $today);
+    $hash = $name . $surname . $age . $dob;
 
-        if (!isset($data[$hash])) {
-            $iCount++;
-            $data[$hash] = true;
-            $id = $iCount;
-            $initials = getinitials($name);
-            fputcsv($file, [$id, $name, $surname, $initials, $age, $dob]);
-            header("Location: /index.php?success=true");
-        }
+    if (!isset($data[$hash])) {
+        $iCount++;
+        $data[$hash] = true;
+        $id = $iCount;
+        $initials = getinitials($name);
+        fputcsv($file, [$id, $name, $surname, $initials, $age, $dob]);
+        header("Location: /index.php?success=true");
     }
 }
 
